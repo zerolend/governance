5dcfce6428e0abcca06efd5f1b075cd0dcf62308
ZeroLend <hello@zerolend.xyz>
2023-08-17 03:50:51 +0300

    commit code

---
+++ deploy/deploy-multisig.ts
@@ -0,0 +1,80 @@
+import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// load env file
+import dotenv from "dotenv";
+dotenv.config();
+
+// load wallet private key from env file
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
+
+if (!PRIVATE_KEY)
+  throw "⛔️ Private key not detected! Add it to the .env file!";
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Greeter contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet(PRIVATE_KEY);
+
+  // Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("MultiSigWallet");
+
+  // Estimate contract deployment fee
+  const greeting = "Hi there!";
+  const args = [
+    [
+      "0xb76f765a785eca438e1d95f594490088afaf9acc",
+      "0x09869df9b616b3e60e7782f6a675d135f17051fd",
+    ],
+    2,
+  ];
+  const deploymentFee = await deployer.estimateDeployFee(artifact, args);
+
+  // ⚠️ OPTIONAL: You can skip this block if your account already has funds in L2
+  // Deposit funds to L2
+  // const depositHandle = await deployer.zkWallet.deposit({
+  //   to: deployer.zkWallet.address,
+  //   token: utils.ETH_ADDRESS,
+  //   amount: deploymentFee.mul(2),
+  // });
+  // // Wait until the deposit is processed on zkSync
+  // await depositHandle.wait();
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+
+  const greeterContract = await deployer.deploy(artifact, args);
+
+  //obtain the Constructor Arguments
+  console.log(
+    "Constructor args:" + greeterContract.interface.encodeDeploy(args)
+  );
+
+  // Show the contract info.
+  const contractAddress = greeterContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+
+  // verify contract for tesnet & mainnet
+  if (process.env.NODE_ENV != "test") {
+    // Contract MUST be fully qualified name (e.g. path/sourceName:contractName)
+    const contractFullyQualifedName =
+      "contracts/MultiSigWallet.sol:MultiSigWallet";
+
+    // Verify contract programmatically
+    const verificationId = await hre.run("verify:verify", {
+      address: contractAddress,
+      contract: contractFullyQualifedName,
+      constructorArguments: args,
+      bytecode: artifact.bytecode,
+    });
+  } else {
+    console.log(`Contract not verified, deployed locally.`);
+  }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+import "@matterlabs/hardhat-zksync-verify";
+
+// dynamically changes endpoints for local tests
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+      }
+    : {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "goerli",
+        zksync: true,
+        // contract verification endpoint
+        verifyURL:
+          "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      };
+
+const config: HardhatUserConfig = {
+  zksolc: {
+    version: "latest",
+    settings: {},
+  },
+  defaultNetwork: "zkSyncEra",
+  networks: {
+    hardhat: {
+      zksync: false,
+    },
+    zkSyncTestnet,
+    zkSyncEra: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      chainId: 324,
+      // contract verification endpoint
+      verifyURL:
+        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.4.15",
+      },
+      {
+        version: "0.8.20",
+      },
+    ],
+  },
+};
+
+export default config;
