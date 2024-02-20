7f09a34c4a9c088bf8f1bc5eee0f3897583221f6
M3gA-Mind <megamind@mahadao.com>
2024-02-20 17:10:12 +0530

    Unit testing for deployment of Lending

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,85 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-dependency-compiler";
+import "hardhat-abi-exporter";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  abiExporter: {
+    path: "./generated-abi",
+    runOnCompile: true,
+    clear: true,
+    // flat: true,
+    spacing: 2,
+    // pretty: true,
+    // format: "minimal",
+  },
+  dependencyCompiler: {
+    paths: [
+      "@zerolendxyz/core-v3/contracts/mocks/helpers/MockIncentivesController.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/helpers/MockReserveConfiguration.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/oracle/CLAggregators/MockAggregator.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/tokens/MintableERC20.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/flashloan/MockFlashLoanReceiver.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/tokens/WETH9Mocked.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/upgradeability/MockVariableDebtToken.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/upgradeability/MockAToken.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/upgradeability/MockStableDebtToken.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/upgradeability/MockInitializableImplementation.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/configuration/PoolAddressesProviderRegistry.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/configuration/PoolAddressesProvider.sol",
+      "@zerolendxyz/core-v3/contracts/misc/AaveOracle.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/tokenization/AToken.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/tokenization/DelegationAwareAToken.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/tokenization/StableDebtToken.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/tokenization/VariableDebtToken.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/GenericLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/ValidationLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/ReserveLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/SupplyLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/EModeLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/BorrowLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/BridgeLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/FlashLoanLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/logic/CalldataLogic.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/pool/Pool.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/pool/L2Pool.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/pool/PoolConfigurator.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/pool/DefaultReserveInterestRateStrategy.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/libraries/aave-upgradeability/InitializableImmutableAdminUpgradeabilityProxy.sol",
+      "@zerolendxyz/core-v3/contracts/dependencies/openzeppelin/upgradeability/InitializableAdminUpgradeabilityProxy.sol",
+      "@zerolendxyz/core-v3/contracts/deployments/ReservesSetupHelper.sol",
+      "@zerolendxyz/core-v3/contracts/misc/AaveProtocolDataProvider.sol",
+      "@zerolendxyz/core-v3/contracts/misc/L2Encoder.sol",
+      "@zerolendxyz/core-v3/contracts/protocol/configuration/ACLManager.sol",
+      "@zerolendxyz/core-v3/contracts/dependencies/weth/WETH9.sol",
+      "@zerolendxyz/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20Detailed.sol",
+      "@zerolendxyz/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/oracle/PriceOracle.sol",
+      "@zerolendxyz/core-v3/contracts/mocks/tokens/MintableDelegationERC20.sol",
+      "@zerolendxyz/periphery-v3/contracts/mocks/testnet-helpers/TestnetERC20.sol",
+    ],
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.12",
+        settings: {
+          optimizer: { enabled: true, runs: 100_000 },
+          evmVersion: "berlin",
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: { enabled: true, runs: 100_000 },
+        },
+      },
+    ],
+  },
   networks: {
     hardhat: {
       forking: {
---
+++ test/fixtures/lending.ts
@@ -0,0 +1,197 @@
+import { ethers } from "hardhat";
+import { parseUnits } from "ethers";
+
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+export const e18 = BigInt(10) ** 18n;
+
+export async function deployLendingPool() {
+  // Contracts are deployed using the first signer/account by default
+  const [owner, otherAccount, vault] = await ethers.getSigners();
+
+  // factories
+  const SupplyLogic = await ethers.getContractFactory("SupplyLogic");
+  const BorrowLogic = await ethers.getContractFactory("BorrowLogic");
+  const EModeLogic = await ethers.getContractFactory("EModeLogic");
+  const BridgeLogic = await ethers.getContractFactory("BridgeLogic");
+  const ConfiguratorLogic = await ethers.getContractFactory(
+    "ConfiguratorLogic"
+  );
+  const PoolLogic = await ethers.getContractFactory("PoolLogic");
+  const DefaultReserveInterestRateStrategy = await ethers.getContractFactory(
+    "DefaultReserveInterestRateStrategy"
+  );
+  const AaveProtocolDataProvider = await ethers.getContractFactory(
+    "AaveProtocolDataProvider"
+  );
+  const LiquidationLogic = await ethers.getContractFactory("LiquidationLogic");
+  const TestnetERC20 = await ethers.getContractFactory("TestnetERC20");
+  const ACLManager = await ethers.getContractFactory("ACLManager");
+  const AaveOracle = await ethers.getContractFactory("AaveOracle");
+  const MockAggregator = await ethers.getContractFactory(
+    "contracts/tests/MockAggregator.sol:MockAggregator"
+  );
+  const PoolAddressesProvider = await ethers.getContractFactory(
+    "PoolAddressesProvider"
+  );
+  const AToken = await ethers.getContractFactory("AToken");
+  const StableDebtToken = await ethers.getContractFactory("StableDebtToken");
+  const VariableDebtToken = await ethers.getContractFactory(
+    "VariableDebtToken"
+  );
+
+  const addressesProvider = await PoolAddressesProvider.deploy(
+    "0",
+    owner.address
+  );
+
+  const protocolDataProvider = await AaveProtocolDataProvider.deploy(
+    addressesProvider.target
+  );
+  const borrowLogic = await BorrowLogic.deploy();
+  const bridgeLogic = await BridgeLogic.deploy();
+  const configuratorLogic = await ConfiguratorLogic.deploy();
+  const eModeLogic = await EModeLogic.deploy();
+  const liquidationLogic = await LiquidationLogic.deploy();
+  const poolLogic = await PoolLogic.deploy();
+  const supplyLogic = await SupplyLogic.deploy();
+
+  const FlashLoanLogic = await ethers.getContractFactory("FlashLoanLogic", {
+    libraries: {
+      BorrowLogic: borrowLogic.target,
+    },
+  });
+  const flashLoanLogic = await FlashLoanLogic.deploy();
+
+  const Pool = await ethers.getContractFactory("Pool", {
+    libraries: {
+      BorrowLogic: borrowLogic.target,
+      BridgeLogic: bridgeLogic.target,
+      EModeLogic: eModeLogic.target,
+      FlashLoanLogic: flashLoanLogic.target,
+      LiquidationLogic: liquidationLogic.target,
+      PoolLogic: poolLogic.target,
+      SupplyLogic: supplyLogic.target,
+    },
+  });
+
+  const PoolConfigurator = await ethers.getContractFactory("PoolConfigurator", {
+    libraries: {
+      ConfiguratorLogic: configuratorLogic.target,
+    },
+  });
+
+  // setup tokens and mock aggregator
+  const erc20 = await TestnetERC20.deploy("WETH", "WETH", 18, owner.address);
+  const mockAggregator = await MockAggregator.deploy(1800 * 1e8, owner.address);
+
+  // 2. Set the MarketId
+  await addressesProvider.setMarketId("Testnet");
+
+  // deploy pool
+  const poolImpl = await Pool.deploy(addressesProvider.target);
+  await poolImpl.initialize(addressesProvider.target);
+
+  // deploy pool configuratior
+  const poolConfiguratorImpl = await PoolConfigurator.deploy();
+  await poolConfiguratorImpl.initialize(addressesProvider.target);
+
+  // deploy acl manager
+  await addressesProvider.setACLAdmin(owner.address);
+  const aclManager = await ACLManager.deploy(addressesProvider.target);
+  await addressesProvider.setACLManager(aclManager.target);
+  await aclManager.addPoolAdmin(owner.address);
+  await aclManager.addEmergencyAdmin(owner.address);
+
+  // deploy oracle
+  const oracle = await AaveOracle.deploy(
+    addressesProvider.target,
+    [erc20.target],
+    [mockAggregator.target],
+    ZERO_ADDRESS,
+    ZERO_ADDRESS,
+    parseUnits("1", 8)
+  );
+  await addressesProvider.setPriceOracle(oracle);
+
+  // set pool impl
+  await addressesProvider.setPoolImpl(poolImpl.target);
+  await addressesProvider.setPoolConfiguratorImpl(poolConfiguratorImpl.target);
+
+  const pool = await ethers.getContractAt(
+    "Pool",
+    await addressesProvider.getPool()
+  );
+  const configurator = await ethers.getContractAt(
+    "PoolConfigurator",
+    await addressesProvider.getPoolConfigurator()
+  );
+
+  const aToken = await AToken.deploy(pool.target);
+  const stableDebtToken = await StableDebtToken.deploy(pool.target);
+  const variableDebtToken = await VariableDebtToken.deploy(pool.target);
+
+  const strategyData = {
+    name: "rateStrategyVolatileOne",
+    optimalUsageRatio: parseUnits("0.45", 27).toString(),
+    baseVariableBorrowRate: "0",
+    variableRateSlope1: parseUnits("0.07", 27).toString(),
+    variableRateSlope2: parseUnits("3", 27).toString(),
+    stableRateSlope1: parseUnits("0.07", 27).toString(),
+    stableRateSlope2: parseUnits("3", 27).toString(),
+    baseStableRateOffset: parseUnits("0.02", 27).toString(),
+    stableRateExcessOffset: parseUnits("0.05", 27).toString(),
+    optimalStableToTotalDebtRatio: parseUnits("0.2", 27).toString(),
+  };
+
+  const strategy = await DefaultReserveInterestRateStrategy.deploy(
+    addressesProvider.target,
+    strategyData.optimalUsageRatio,
+    strategyData.baseVariableBorrowRate,
+    strategyData.variableRateSlope1,
+    strategyData.variableRateSlope2,
+    strategyData.stableRateSlope1,
+    strategyData.stableRateSlope2,
+    strategyData.baseStableRateOffset,
+    strategyData.stableRateExcessOffset,
+    strategyData.optimalStableToTotalDebtRatio
+  );
+
+  await configurator.initReserves([
+    {
+      aTokenImpl: aToken.target,
+      stableDebtTokenImpl: stableDebtToken.target,
+      variableDebtTokenImpl: variableDebtToken.target,
+      underlyingAssetDecimals: 18,
+      interestRateStrategyAddress: strategy.target,
+      underlyingAsset: erc20.target,
+      treasury: ZERO_ADDRESS,
+      incentivesController: ZERO_ADDRESS,
+      aTokenName: `ZeroLend z0 TEST`,
+      aTokenSymbol: `z0TEST`,
+      variableDebtTokenName: `ZeroLend Variable Debt TEST`,
+      variableDebtTokenSymbol: `variableDebtTEST`,
+      stableDebtTokenName: `ZeroLend Stable Debt TEST`,
+      stableDebtTokenSymbol: `stableDebtTEST}`,
+      params: "0x10",
+    },
+  ]);
+
+  await configurator.setReserveBorrowing(erc20.target, true);
+  await configurator.configureReserveAsCollateral(
+    erc20.target,
+    8000,
+    8250,
+    10500
+  );
+
+  return {
+    owner,
+    configurator,
+    erc20,
+    pool,
+    addressesProvider,
+    aclManager,
+    protocolDataProvider,
+  };
+}
