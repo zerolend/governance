1f90dab2b9282606f7e103d2d9edfc27ba8bbbbb
M3gA-Mind <megamind@mahadao.com>
2024-03-05 15:51:38 +0530

    minor change

--- contracts/voter/EligibilityCriteria.sol
+++ contracts/voter/EligibilityCriteria.sol
@@ -18,8 +18,6 @@ import {IAaveOracle} from "@zerolendxyz/core-v3/contracts/interfaces/IAaveOracle
 import {IEligibilityCriteria} from "../interfaces/IEligibilityCriteria.sol";
 import {VersionedInitializable} from "../proxy/VersionedInitializable.sol";
 
-import 'hardhat/console.sol';
-
 contract EligibilityCriteria is VersionedInitializable, IEligibilityCriteria {
     IVotes public staking;
     IAaveOracle public oracle;
@@ -30,8 +28,6 @@ contract EligibilityCriteria is VersionedInitializable, IEligibilityCriteria {
         address _oracle,
         address _zero
     )external {
-    // ) external initializer {
-        console.log('Hit');
         staking = IVotes(_staking);
         oracle = IAaveOracle(_oracle);
         zero = _zero;
