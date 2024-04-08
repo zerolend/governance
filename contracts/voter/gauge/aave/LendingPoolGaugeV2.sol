9f2ec7c04f871027727f939baddef4b475ebc3f9
Deadshot Ryker <ryker@zerolend.xyz>
2024-04-08 22:31:08 +0530

    fixed bugs and simplified the logic

--- contracts/voter/gauge/aave/LendingPoolGaugeV2.sol
+++ contracts/voter/gauge/aave/LendingPoolGaugeV2.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.12;
 
-import {IEmissionManager} from "@zerolendxyz/periphery-v3/contracts/rewards/interfaces/IEmissionManager.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IEmissionManager, ITransferStrategyBase, IEACAggregatorProxy} from "@zerolendxyz/periphery-v3/contracts/rewards/interfaces/IEmissionManager.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPoolDataProvider} from "@zerolendxyz/core-v3/contracts/interfaces/IPoolDataProvider.sol";
 import {IRewardDistributor} from "../../../interfaces/IRewardDistributor.sol";
@@ -10,34 +11,53 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 
 // Gauges are used to incentivize pools, they emit reward tokens over 14 days for staked LP tokens
 // Nuance: getReward must be called at least once for tokens other than incentive[0] to start accrueing rewards
-contract LendingPoolGaugeV2 is IRewardDistributor {
+contract LendingPoolGaugeV2 is Ownable, IRewardDistributor {
     using SafeERC20 for IERC20;
 
-    address public immutable z0Token;
-    address public immutable z0TokenDebt;
+    address immutable asset;
+    IPoolDataProvider immutable dataProvider;
     IEmissionManager public immutable emissionManagerProxy;
-    uint256 public duration;
+
     address public oracle;
-    address public strategy;
+    address public voter;
+    address public z0Token;
+    address public z0TokenDebt;
+    ITransferStrategyBase public strategy;
+    uint32 public duration;
+
+    event AmountNotified(address _asset, uint256 _amount);
 
     constructor(
         address _asset,
         address _oracle,
-        address _strategy,
-        uint256 _duration,
+        address _voter,
         IEmissionManager _emissionManagerProxy,
-        IPoolDataProvider _provider
-    ) {
+        IPoolDataProvider _data,
+        ITransferStrategyBase _strategy,
+        uint32 _duration
+    ) Ownable(msg.sender) {
         emissionManagerProxy = _emissionManagerProxy;
-        (z0Token, z0TokenDebt, ) = _provider.getReserveTokensAddresses(_asset);
-        setConfig(_duration, _oracle, _strategy);
+        voter = _voter;
+        asset = _asset;
+        dataProvider = _data;
+        updateData(_oracle, _strategy, _duration);
     }
 
-    function setConfig(
-        uint256 _duration,
+    function updateData(
         address _oracle,
-        address _strategy
+        ITransferStrategyBase _strategy,
+        uint32 _duration
     ) public onlyOwner {
+        // get data from pool provider
+        (z0Token, z0TokenDebt, ) = dataProvider.getReserveTokensAddresses(
+            asset
+        );
+        (, , , , , , bool borrowingEnabled, , , ) = dataProvider
+            .getReserveConfigurationData(asset);
+
+        // if there is no borrowing enabled then reduce to 0
+        if (!borrowingEnabled) z0TokenDebt = address(0);
+
         duration = _duration;
         oracle = _oracle;
         strategy = _strategy;
@@ -47,42 +67,56 @@ contract LendingPoolGaugeV2 is IRewardDistributor {
         address token,
         uint256 amount
     ) external returns (bool) {
+        require(msg.sender == voter, "!voter");
+
         // send tokens to the transfer strategy
         IERC20(token).safeTransferFrom(msg.sender, address(strategy), amount);
 
-        uint256 distributionEnd = block.timestamp + duration;
-        uint256 emissionPerSecond = amount / _duration;
+        uint32 distributionEnd = uint32(block.timestamp) + duration;
+        uint88 emissionPerSecond = uint88(amount / duration);
 
-        // send 1/4 to the supply side
-        uint256 emissionPerSecondSupply = emissionPerSecond / 4;
+        // send 1/4 to the supply side if there is no debt. else send all the rewards to the supply side
+        uint88 emissionPerSecondSupply = z0TokenDebt != address(0)
+            ? emissionPerSecond / 4
+            : emissionPerSecond;
 
-        // send 3/4th to the borrow side
-        uint256 emissionPerSecondDebt = emissionPerSecond * 3 / 4
+        // send 3/4th to the borrow side if there is a debt token. else send 0
+        uint88 emissionPerSecondDebt = z0TokenDebt != address(0)
+            ? (emissionPerSecond * 3) / 4
+            : 0;
 
-        // config the params now
-        emissionManagerProxy.configureAssets(
-            [
-                RewardsDataTypes.RewardsConfigInput({
-                    emissionPerSecond: emissionPerSecondSupply,
-                    totalSupply: 0,
-                    distributionEnd: distributionEnd,
-                    asset: z0Token,
-                    reward: token,
-                    transferStrategy: strategy,
-                    rewardOracle: oracle
-                }),
-                RewardsDataTypes.RewardsConfigInput({
-                    emissionPerSecond: emissionPerSecondDebt,
-                    totalSupply: 0,
-                    distributionEnd: distributionEnd,
-                    asset: z0TokenDebt,
-                    reward: token,
-                    transferStrategy: strategy,
-                    rewardOracle: oracle
-                })
-            ]
-        );
+        // config the params for the emission manager
+        RewardsDataTypes.RewardsConfigInput[]
+            memory data = new RewardsDataTypes.RewardsConfigInput[](
+                z0TokenDebt != address(0) ? 2 : 1
+            );
 
+        data[0] = RewardsDataTypes.RewardsConfigInput({
+            emissionPerSecond: emissionPerSecondSupply,
+            totalSupply: 0,
+            distributionEnd: distributionEnd,
+            asset: z0Token,
+            reward: token,
+            transferStrategy: strategy,
+            rewardOracle: IEACAggregatorProxy(oracle)
+        });
+
+        if (emissionPerSecondDebt > 0) {
+            data[1] = RewardsDataTypes.RewardsConfigInput({
+                emissionPerSecond: emissionPerSecondDebt,
+                totalSupply: 0,
+                distributionEnd: distributionEnd,
+                asset: z0TokenDebt,
+                reward: token,
+                transferStrategy: strategy,
+                rewardOracle: IEACAggregatorProxy(oracle)
+            });
+        }
+
+        // send it!
+        emissionManagerProxy.configureAssets(data);
+
+        emit AmountNotified(token, amount);
         return true;
     }
 }
