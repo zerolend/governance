b482c7a4f535185ea9d0bd270221214303d72656
nikhilbajaj31 <nikhil@zerolend.xyz>
2024-04-10 16:01:14 +0530

    feat: tracking reward claims

--- contracts/airdrop/AirdropRewarder.sol
+++ contracts/airdrop/AirdropRewarder.sol
@@ -13,59 +13,61 @@ contract AirdropRewarder is Initializable, OwnableUpgradeable {
     address public safeAddress;
     bytes32 private merkleRoot;
 
-    mapping(address => mapping(address => uint256)) private totalRewardsClaimed;
+    ERC20Upgradeable public rewardToken;
+
+    mapping(address => bool) public rewardsClaimed;
 
     error InvalidAddress();
+    error RewardsAlreadyClaimed();
     error InvalidMerkleProof(bytes32[]);
 
     event MerkleRootSet(bytes32 oldMerkleRoot, bytes32 newMerkleRoot);
+    event RewardTokenSet(address oldRewardToken, address newRewardToken);
     event SafeAddressSet(address _safeAddress);
-    event RewardsClaimed(
-        address _user,
-        address _rewardsToken,
-        uint256 _rewardsAmount
-    );
+    event RewardsClaimed(address _user, uint256 _rewardsAmount);
     event RewardTerminated(address _rewardAddress);
 
-    function initialize(address _safeAddress, bytes32 _merkleRoot) external initializer {
+    function initialize(
+        address _safeAddress,
+        bytes32 _merkleRoot,
+        address _rewardToken
+    ) external initializer {
         __Ownable_init(msg.sender);
         safeAddress = _safeAddress;
         merkleRoot = _merkleRoot;
+        rewardToken = ERC20Upgradeable(_rewardToken);
         emit SafeAddressSet(_safeAddress);
     }
 
-    function setMerkleRoot(
-        bytes32 _merkleRoot
-    ) external onlyOwner {
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
         emit MerkleRootSet(_merkleRoot, merkleRoot);
         merkleRoot = _merkleRoot;
     }
 
+    function setRewardToken(address _rewardToken) external onlyOwner {
+        if (_rewardToken == address(0)) revert InvalidAddress();
+        emit RewardTokenSet(_rewardToken, address(rewardToken));
+        rewardToken = ERC20Upgradeable(_rewardToken);
+    }
+
     function claim(
         address _user,
-        address rewardToken,
-        uint256 _totalClaimableAmount,
+        uint256 _claimAmount,
         bytes32[] calldata _merkleProofs
     ) external {
-        uint256 totalClaimableAmount = _totalClaimableAmount;
-        address rewardAddress = rewardToken;
-        if (rewardAddress == address(0)) revert InvalidAddress();
+        if (_user == address(0)) revert InvalidAddress();
 
-        bytes32 node = keccak256(abi.encodePacked(_user, totalClaimableAmount));
-        if (
-            !MerkleProof.verify(_merkleProofs, merkleRoot, node)
-        ) revert InvalidMerkleProof(_merkleProofs);
-        uint256 claimableAmount = totalClaimableAmount -
-            totalRewardsClaimed[_user][rewardAddress];
+        bytes32 node = keccak256(abi.encodePacked(_user, _claimAmount));
 
-        if (claimableAmount > 0) {
-            totalRewardsClaimed[_user][rewardAddress] = totalClaimableAmount;
-            ERC20Upgradeable(rewardAddress).safeTransfer(
-                _user,
-                claimableAmount
-            );
-        }
-        emit RewardsClaimed(_user, rewardToken, _totalClaimableAmount);
+        if (!MerkleProof.verify(_merkleProofs, merkleRoot, node))
+            revert InvalidMerkleProof(_merkleProofs);
+
+        if (rewardsClaimed[_user]) revert RewardsAlreadyClaimed();
+        
+        rewardsClaimed[_user] = true;
+        rewardToken.safeTransfer(_user, _claimAmount);
+        
+        emit RewardsClaimed(_user, _claimAmount);
     }
 
     function setSafeAddress(address _safeAddress) external onlyOwner {
@@ -76,22 +78,10 @@ contract AirdropRewarder is Initializable, OwnableUpgradeable {
     function terminateReward(address _rewardAddress) public onlyOwner {
         if (_rewardAddress == address(0)) revert InvalidAddress();
 
-        ERC20Upgradeable(_rewardAddress).safeTransfer(
+        rewardToken.safeTransfer(
             safeAddress,
             ERC20Upgradeable(_rewardAddress).balanceOf(address(this))
         );
         emit RewardTerminated(_rewardAddress);
-    }
-
-    function getAmountsClaimedForUser(
-        address _user,
-        address[] calldata _rewardToken
-    ) external view returns (uint256[] memory) {
-        uint256 length = _rewardToken.length;
-        uint256[] memory amountsClaimed = new uint256[](length);
-        for (uint256 i = 0; i < length; i++) {
-            amountsClaimed[i] = totalRewardsClaimed[_user][_rewardToken[i]];
-        }
-        return amountsClaimed;
     }
 }
--- test/AirdropRewarder.test.ts
+++ test/AirdropRewarder.test.ts
@@ -2,7 +2,10 @@ import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 import { AirdropRewarder } from "../typechain-types";
 import { deploy, deployProxy } from "./airdrop-utils/deploy";
 
-import { BalanceLeaf, BalanceTree } from "./airdrop-utils/merkle-tree/BalanceTree";
+import {
+  BalanceLeaf,
+  BalanceTree,
+} from "./airdrop-utils/merkle-tree/BalanceTree";
 import { ethers } from "hardhat";
 import { AddressLike, Contract, parseEther } from "ethers";
 import { expect } from "chai";
@@ -21,25 +24,26 @@ describe("Airdrop Tests", async () => {
   beforeEach("Deploy Airdrop Contracts", async () => {
     accounts = await ethers.getSigners();
     [owner, user1, user2, user3] = accounts;
-    leaves = [user1.address, user2.address].map(
-      (account, i) => ({
-        account: <AddressLike>account,
-        amount: parseEther((i + 1).toString()), // user1: 1, user2: 2
-      })
-    );
+    leaves = [user1.address, user2.address].map((account, i) => ({
+      account: <AddressLike>account,
+      amount: parseEther((i + 1).toString()), // user1: 1, user2: 2
+    }));
     tree = new BalanceTree(leaves);
-    airdropRewarder = (await deployProxy(
-      "AirdropRewarder",
-      "initialize(address)",
-      user3.address
-    )) as unknown as AirdropRewarder;
 
     airdropToken = await deploy("MockReward", owner.address);
     await airdropToken.waitForDeployment();
 
+    airdropRewarder = (await deployProxy(
+      "AirdropRewarder",
+      "initialize(address,bytes32,address)",
+      user3.address,
+      tree.getHexRoot(),
+      airdropToken.target
+    )) as unknown as AirdropRewarder;
+
     await airdropToken.mint(airdropRewarder.target, parseEther("1000"));
 
-    await airdropRewarder.setMerkleRoot(tree.getHexRoot(), airdropToken.target);
+    await airdropRewarder.setMerkleRoot(tree.getHexRoot());
   });
 
   it("Cannot claim with wrong amount", async () => {
@@ -47,7 +51,7 @@ describe("Airdrop Tests", async () => {
     await expect(
       airdropRewarder
         .connect(user1)
-        .claim(leaves[0].account, airdropToken.target, parseEther("0.5"), proof)
+        .claim(leaves[0].account, parseEther("0.5"), proof)
     ).to.be.revertedWithCustomError(airdropRewarder, "InvalidMerkleProof");
   });
 
@@ -56,59 +60,52 @@ describe("Airdrop Tests", async () => {
     await expect(
       airdropRewarder
         .connect(user1)
-        .claim(leaves[1].account, airdropToken.target, parseEther("2"), proof)
+        .claim(leaves[1].account, parseEther("2"), proof)
     ).to.be.revertedWithCustomError(airdropRewarder, "InvalidMerkleProof");
   });
 
   it("Cannot claim for address other proof", async () => {
     const proof1 = tree.getProof(leaves[1].account, leaves[1].amount);
     await expect(
-      airdropRewarder.claim(
-        leaves[0].account,
-        airdropToken.target,
-        parseEther("2"),
-        proof1
-      )
+      airdropRewarder.claim(leaves[0].account, parseEther("2"), proof1)
     ).to.be.revertedWithCustomError(airdropRewarder, "InvalidMerkleProof");
   });
 
   it("Can claim successfully", async () => {
     const proof0 = tree.getProof(leaves[0].account, leaves[0].amount);
     await expect(
-      airdropRewarder.claim(
-        leaves[0].account,
-        airdropToken.target,
-        parseEther("1"),
-        proof0
-      )
+      airdropRewarder.claim(leaves[0].account, parseEther("1"), proof0)
     )
       .to.emit(airdropRewarder, "RewardsClaimed")
-      .withArgs(leaves[0].account, airdropToken.target, parseEther("1"));
+      .withArgs(leaves[0].account, parseEther("1"));
+  });
+
+  it("Cannot claim again", async () => {
+    const proof0 = tree.getProof(leaves[0].account, leaves[0].amount);
+    await expect(
+      airdropRewarder.claim(leaves[0].account, parseEther("1"), proof0)
+    )
+      .to.emit(airdropRewarder, "RewardsClaimed")
+      .withArgs(leaves[0].account, parseEther("1"));
+    await expect(
+      airdropRewarder.claim(leaves[0].account, parseEther("1"), proof0)
+    )
+      .to.be.revertedWithCustomError(airdropRewarder, "RewardsAlreadyClaimed")
   });
 
   it("can claim for multiple users", async () => {
     const proof0 = tree.getProof(leaves[0].account, leaves[0].amount);
     await expect(
-      airdropRewarder.claim(
-        leaves[0].account,
-        airdropToken.target,
-        parseEther("1"),
-        proof0
-      )
+      airdropRewarder.claim(leaves[0].account, parseEther("1"), proof0)
     )
       .to.emit(airdropRewarder, "RewardsClaimed")
-      .withArgs(leaves[0].account, airdropToken.target, parseEther("1"));
+      .withArgs(leaves[0].account, parseEther("1"));
 
     const proof1 = tree.getProof(leaves[1].account, leaves[1].amount);
     await expect(
-      airdropRewarder.claim(
-        leaves[1].account,
-        airdropToken.target,
-        parseEther("2"),
-        proof1
-      )
+      airdropRewarder.claim(leaves[1].account, parseEther("2"), proof1)
     )
       .to.emit(airdropRewarder, "RewardsClaimed")
-      .withArgs(leaves[1].account, airdropToken.target, parseEther("2"));
+      .withArgs(leaves[1].account, parseEther("2"));
   });
 });
