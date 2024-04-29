4c3fc684d18473fd61ff8869c79f6a7c7e6f2602
oxoxDev <nikhil@zerolend.xyz>
2024-04-30 00:38:03 +0530

    unit test to get rewards

--- contracts/locker/OmnichainStaking.sol
+++ contracts/locker/OmnichainStaking.sol
@@ -13,12 +13,15 @@ pragma solidity ^0.8.20;
 // Twitter: https://twitter.com/zerolendxyz
 
 import {OApp} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Votes} from "@openzeppelin/contracts/governance/utils/Votes.sol";
 import {IOmnichainStaking} from "../interfaces/IOmnichainStaking.sol";
 import {ILocker} from "../interfaces/ILocker.sol";
 import {IZeroLend} from "../interfaces/IZeroLend.sol";
 import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 
 
 /**
@@ -26,14 +29,14 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
  * @dev An omnichain staking contract that allows users to stake their veNFT
  * and get some voting power. Once staked, the voting power is available cross-chain.
  */
-contract OmnichainStaking is IOmnichainStaking, ERC20VotesUpgradeable, ReentrancyGuard {
+contract OmnichainStaking is IOmnichainStaking, ERC20VotesUpgradeable, ReentrancyGuard, OwnableUpgradeable {
     ILocker public lpLocker;
     ILocker public tokenLocker;
     IZeroLend public rewardsToken;
 
-    uint256 public periodFinish = 0;
-    uint256 public rewardRate = 0;
-    uint256 public rewardsDuration = 7 days;
+    uint256 public periodFinish;
+    uint256 public rewardRate;
+    uint256 public rewardsDuration;
     uint256 public lastUpdateTime;
     uint256 public rewardPerTokenStored;
 
@@ -48,6 +51,9 @@ contract OmnichainStaking is IOmnichainStaking, ERC20VotesUpgradeable, Reentranc
     error InvalidUnstaker(address, address);
 
     event RewardPaid(address indexed user, uint256 reward);
+    event RewardAdded(uint256 reward);
+    event Recovered(address token, uint256 amount);
+    event RewardsDurationUpdated(uint256 newDuration);
 
     // constructor() {
     //     _disableInitializers();
@@ -66,6 +72,7 @@ contract OmnichainStaking is IOmnichainStaking, ERC20VotesUpgradeable, Reentranc
     ) external initializer {
         // TODO add LZ
         __ERC20Votes_init();
+        __Ownable_init(msg.sender);
         __ERC20_init("ZERO Voting Power", "ZEROvp");
 
         tokenLocker = ILocker(_tokenLocker);
@@ -230,7 +237,8 @@ contract OmnichainStaking is IOmnichainStaking, ERC20VotesUpgradeable, Reentranc
      * @return The last time rewards were applicable.
      */
     function lastTimeRewardApplicable() public view returns (uint256) {
-        return block.timestamp < periodFinish ? block.timestamp : periodFinish;
+        uint256 time = block.timestamp < periodFinish ? block.timestamp : periodFinish;
+        return time;
     }
 
     /**
@@ -241,13 +249,41 @@ contract OmnichainStaking is IOmnichainStaking, ERC20VotesUpgradeable, Reentranc
         if (totalSupply() == 0) {
             return rewardPerTokenStored;
         }
-        return
-            rewardPerTokenStored +
-            ((lastTimeRewardApplicable() - lastUpdateTime) *
-                rewardRate *
-                1e18) /
-            totalSupply();
+
+        uint256 timeElapsed = lastTimeRewardApplicable() - lastUpdateTime;
+        uint256 rewardPerTokenChange = (timeElapsed * rewardRate * 1e18) / totalSupply();
+
+        return rewardPerTokenStored + rewardPerTokenChange;
     }
+
+
+    function notifyRewardAmount(uint256 reward) external onlyOwner updateReward(address(0)) {
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward/rewardsDuration;
+        } else {
+            uint256 remaining = periodFinish- block.timestamp;
+            uint256 leftover = remaining * rewardRate;
+            rewardRate = (reward+leftover)/rewardsDuration;
+        }
+
+        // Ensure the provided reward amount is not more than the balance in the contract.
+        // This keeps the reward rate in the right range, preventing overflows due to
+        // very high values of rewardRate in the earned and rewardsPerToken functions;
+        // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+        uint balance = rewardsToken.balanceOf(address(this));
+        require(rewardRate <= balance/rewardsDuration, "Provided reward too high");
+
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp + rewardsDuration;
+        emit RewardAdded(reward);
+    }
+
+    function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
+        require(tokenAddress != address(rewardsToken), "Cannot withdraw the staking token");
+        IERC20(tokenAddress).transfer(owner(), tokenAmount);
+        emit Recovered(tokenAddress, tokenAmount);
+    }
+
 
     /**
      * @dev Transfers rewards to the caller.
@@ -322,6 +358,15 @@ contract OmnichainStaking is IOmnichainStaking, ERC20VotesUpgradeable, Reentranc
             rewards[account] = earned(account);
             userRewardPerTokenPaid[account] = rewardPerTokenStored;
         }
+    }
+
+    function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
+        require(
+            block.timestamp > periodFinish,
+            "Previous rewards period not finished"
+        );
+        rewardsDuration = _rewardsDuration;
+        emit RewardsDurationUpdated(rewardsDuration);
     }
 
     /**
---
+++ test/OmnichainStakingRewards.test.ts
@@ -0,0 +1,75 @@
+import { expect } from "chai";
+import { deployGovernance } from "./fixtures/governance";
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import {
+  LockerToken,
+  OmnichainStaking,
+  StakingBonus,
+  VestedZeroNFT,
+  ZeroLend,
+} from "../typechain-types";
+import { e18 } from "./fixtures/utils";
+import { ethers } from "hardhat";
+import { parseEther } from "ethers";
+
+describe("Omnichain Staking Unit tests", () => {
+  let ant: SignerWithAddress;
+  let vest: VestedZeroNFT;
+  let now: number;
+  let stakingBonus: StakingBonus;
+  let zero: ZeroLend;
+  let lockerToken: LockerToken;
+  let omniStaking: OmnichainStaking;
+
+  beforeEach(async () => {
+    const deployment = await loadFixture(deployGovernance);
+    ant = deployment.ant;
+    zero = deployment.zero;
+    vest = deployment.vestedZeroNFT;
+    stakingBonus = deployment.stakingBonus;
+    lockerToken = deployment.lockerToken;
+    omniStaking = deployment.omnichainStaking;
+    now = Math.floor(Date.now() / 1000);
+
+    
+    await zero.whitelist(vest.target, true);
+    await zero.whitelist(omniStaking.target, true);
+    await zero.whitelist(stakingBonus.target, true);
+    await zero.whitelist(lockerToken.target, true);
+    
+    await zero.transfer(omniStaking.target, parseEther("100"));
+    
+    await omniStaking.setRewardsDuration(86400 * 14);
+    await omniStaking.notifyRewardAmount(parseEther("1"));
+
+    // deployer should be able to mint a nft for another user
+    await vest.mint(
+      ant.address,
+      e18 * 20n, // 20 ZERO linear vesting
+      0, // 0 ZERO upfront
+      1000, // linear duration - 1000 seconds
+      0, // cliff duration - 0 seconds
+      0, // unlock date
+      true, // penalty -> false
+      0
+    );
+
+    await vest
+      .connect(ant)
+      ["safeTransferFrom(address,address,uint256)"](
+        ant.address,
+        stakingBonus.target,
+        1
+      );
+    expect(await omniStaking.balanceOf(ant.address)).greaterThan(
+      (e18 * 199n) / 10n
+    );
+  });
+
+  it("should give rewards for staking vests", async () => {
+    expect(await zero.balanceOf(ant.address)).to.equal(0);
+    await expect(omniStaking.connect(ant).getReward()).to.emit(omniStaking, "RewardPaid");
+    expect(await zero.balanceOf(ant.address)).to.be.greaterThan(0);
+  })
+});
