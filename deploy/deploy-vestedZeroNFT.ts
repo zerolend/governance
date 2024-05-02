e6354f8dbd64c16b872f28770252931663d5c36f
oxoxDev <nikhil@zerolend.xyz>
2024-05-02 12:40:32 +0530

    deploy vested zero nft

---
+++ deploy/deploy-vestedZeroNFT.ts
@@ -0,0 +1,36 @@
+import { ethers } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const TOKEN_ADDRESS = "";
+const STAKING_BONUS_ADDRESS = "";
+
+async function main(hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  if (
+    TOKEN_ADDRESS.length &&
+    STAKING_BONUS_ADDRESS.length
+  ) {
+    await deploy("VestedZeroNFT", {
+      from: deployer,
+      contract: "VestedZeroNFT",
+      proxy: {
+        owner: deployer,
+        proxyContract: "OpenZeppelinTransparentProxy",
+        execute: {
+          methodName: "init",
+          args: [TOKEN_ADDRESS, STAKING_BONUS_ADDRESS],
+        },
+      },
+      autoMine: true,
+      log: true,
+    });
+  }else {
+    throw new Error("Invalid address for locker/stakingBonus");
+  }
+}
+
+main.tags = ["VestedZeroNFT"];
+export default main;
