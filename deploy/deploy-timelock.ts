4c1cf82ebd4783a18816480cfd94070a48e7272b
oxoxDev <nikhil@zerolend.xyz>
2024-05-02 12:48:18 +0530

    fix: timelock deployment script errors

--- deploy/deploy-timelock.ts
+++ deploy/deploy-timelock.ts
@@ -1,7 +1,7 @@
-import { Wallet, utils } from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+import { Wallet } from "zksync-ethers";
 
 // load env file
 import dotenv from "dotenv";
@@ -14,7 +14,7 @@ if (!PRIVATE_KEY)
   throw "⛔️ Private key not detected! Add it to the .env file!";
 
 // An example of a deploy script that will deploy and call a simple contract.
-export default async function (hre: HardhatRuntimeEnvironment) {
+async function main(hre: HardhatRuntimeEnvironment) {
   console.log(`Running deploy script for the Greeter contract`);
 
   // Initialize the wallet.
@@ -54,7 +54,7 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
   // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
   // `greeting` is an argument for contract constructor.
-  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  const parsedFee = ethers.formatEther(deploymentFee.toString());
   console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
   const greeterContract = await deployer.deploy(artifact, args);
@@ -85,3 +85,6 @@ export default async function (hre: HardhatRuntimeEnvironment) {
     console.log(`Contract not verified, deployed locally.`);
   }
 }
+
+main.tags = ["Timelock"];
+export default main;
