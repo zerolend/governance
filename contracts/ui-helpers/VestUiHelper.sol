ee9cf0a04850f3551facc6c0ced3090a20bfa74f
oxoxDev <nikhil@zerolend.xyz>
2024-05-09 14:13:40 +0530

    feat: ui helper contract

---
+++ contracts/ui-helpers/VestUiHelper.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+// ███████╗███████╗██████╗  ██████╗
+// ╚══███╔╝██╔════╝██╔══██╗██╔═══██╗
+//   ███╔╝ █████╗  ██████╔╝██║   ██║
+//  ███╔╝  ██╔══╝  ██╔══██╗██║   ██║
+// ███████╗███████╗██║  ██║╚██████╔╝
+// ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝
+
+// Website: https://zerolend.xyz
+// Discord: https://discord.gg/zerolend
+// Twitter: https://twitter.com/zerolendxyz
+
+import {VestedZeroNFT} from "../vesting/VestedZeroNFT.sol";
+import {OmnichainStaking} from "../locker/OmnichainStaking.sol";
+import {ILocker} from "../interfaces/ILocker.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/// @title VestedZeroNFT is a NFT based contract to hold all the user vests
+contract VestedZeroUiHelper is Initializable {
+    VestedZeroNFT vestedZero;
+    OmnichainStaking omnichainStaking;
+
+    struct VestDetails {
+        uint256 id;
+        uint256 cliffDuration;
+        uint256 unlockDate;
+        uint256 pendingClaimed;
+        uint256 pending;
+        uint256 upfrontClaimed;
+        uint256 upfront;
+        uint256 linearDuration;
+        uint256 createdAt;
+        bool hasPenalty;
+        VestedZeroNFT.VestCategory category;
+        uint256 claimable;
+        uint256 unClaimed;
+        uint256 penalty;
+    }
+
+    struct LockedBalanceWithApr {
+        uint256 amount;
+        uint256 end;
+        uint256 start;
+        uint256 power;
+        uint256 apr;
+    }
+
+    function initialize(
+        address _vestedZeroNFT,
+        address _omnichainStaking
+    ) external initializer {
+        vestedZero = VestedZeroNFT(_vestedZeroNFT);
+        omnichainStaking = OmnichainStaking(_omnichainStaking);
+    }
+
+    function getVestedNFTData() external view returns (VestDetails[] memory) {
+        uint256 nftsOwned = vestedZero.balanceOf(msg.sender);
+        VestDetails[] memory lockDetails = new VestDetails[](nftsOwned);
+        for (uint i; i < nftsOwned; ) {
+            VestDetails memory lock;
+            uint256 tokenId = vestedZero.tokenOfOwnerByIndex(msg.sender, i);
+            (
+                lock.cliffDuration,
+                lock.unlockDate,
+                lock.pendingClaimed,
+                lock.pending,
+                lock.upfrontClaimed,
+                ,
+                ,
+                ,
+                ,
+
+            ) = vestedZero.tokenIdToLockDetails(tokenId);
+            (
+                ,
+                ,
+                ,
+                ,
+                ,
+                lock.upfront,
+                lock.linearDuration,
+                lock.createdAt,
+                lock.hasPenalty,
+                lock.category
+            ) = vestedZero.tokenIdToLockDetails(tokenId);
+
+            (uint256 upfrontClaimable, uint256 pendingClaimable) = vestedZero
+                .claimable(tokenId);
+
+            lock.id = tokenId;
+            lock.penalty = vestedZero.penalty(tokenId);
+            lock.claimable = upfrontClaimable + pendingClaimable;
+            lock.unClaimed = vestedZero.unclaimed(tokenId);
+
+            lockDetails[i] = lock;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        return lockDetails;
+    }
+
+    function getLockDetails() external view returns (LockedBalanceWithApr[] memory){
+        (
+            uint256[] memory tokenIds,
+            ILocker.LockedBalance[] memory lockedBalances
+        ) = omnichainStaking.getLockedNftDetails(msg.sender);
+        uint256 rewardRate = omnichainStaking.rewardRate();
+        uint256 totalSupply = omnichainStaking.totalSupply();
+
+        uint256 totalTokenIds = tokenIds.length;
+
+        LockedBalanceWithApr[] memory lockDetails = new LockedBalanceWithApr[](totalTokenIds);
+
+        for (uint i; i < totalTokenIds; ) {
+            LockedBalanceWithApr memory lock;
+            ILocker.LockedBalance memory lockedBalance = lockedBalances[i];
+            uint256 poolPercentage = (lockedBalances[i].amount * 1000000) / totalSupply;
+            uint256 poolRewardAnnual = rewardRate * 31536000;
+            uint256 rewardsEarned = (poolPercentage * poolRewardAnnual) / 1000000;
+
+            lock.amount = lockedBalance.amount;
+            lock.start = lockedBalance.start;
+            lock.end = lockedBalance.end;
+            lock.power = lockedBalance.power;
+            lock.apr = ((rewardsEarned * 1000)/lockedBalances[i].amount)/1000;
+
+            lockDetails[i] = lock;
+            
+            unchecked {
+                ++i;
+            }
+        }
+
+        return lockDetails;
+    }
+}
---
+++ deploy/deploy-VestUiHelper.ts
@@ -0,0 +1,40 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+// load env file
+import dotenv from "dotenv";
+import { ethers } from "hardhat";
+dotenv.config();
+
+const VESTED_NFT_ADDRESS = "0x394FA2886a3a18b08FDf2C5be2B7977f99d28Feb";
+const OMNICHAIN_STAKING = "0x69865827D1e6aEA50aBD545db2B7Bd34c3b95af7";
+
+const deployAirdropRewarder = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  if (VESTED_NFT_ADDRESS.length && OMNICHAIN_STAKING.length) {
+    const vestui = await deploy("VestedZeroUiHelper", {
+      from: deployer,
+      contract: "VestedZeroUiHelper",
+      proxy: {
+        owner: deployer,
+        proxyContract: "OpenZeppelinTransparentProxy",
+        execute: {
+          methodName: "initialize",
+          args: [VESTED_NFT_ADDRESS, OMNICHAIN_STAKING],
+        },
+      },
+      autoMine: true,
+      log: true,
+    });
+    const contract = await ethers.getContractAt("VestedZeroUiHelper", vestui.address);
+    await contract.initialize(VESTED_NFT_ADDRESS, OMNICHAIN_STAKING);
+  } else {
+    throw new Error("Invalid address for vest");
+  }
+
+};
+
+deployAirdropRewarder.tags = ["VestUiHelper"];
+export default deployAirdropRewarder;
