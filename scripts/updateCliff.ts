0c6336ecc65ef032dfeb0199b1b4a1d88b5e5d55
oxoxDev <nikhil@zerolend.xyz>
2024-05-09 17:03:18 +0530

    feat: script to check for all the nfts and then update cliffs to 3 months

--- scripts/updateCliff.ts
+++ scripts/updateCliff.ts
@@ -1,8 +1,10 @@
 import { ethers } from "hardhat";
+import { VestedZeroNFT } from "../typechain-types";
 
-const VESTED_ZERO_NFT_ADDRESS = "";
-const LINEAR_DURATION = 86400*90;
-const CLIFF_DURATION = 86400*90;
+const VESTED_ZERO_NFT_ADDRESS = "0x9FA72ea96591e486FF065E7C8A89282dEDfA6C12";
+const LINEAR_DURATION = 86400 * 91;
+const CLIFF_DURATION = 86400 * 90;
+const tokenIds: number[] = [];
 
 async function main() {
   if (VESTED_ZERO_NFT_ADDRESS.length && LINEAR_DURATION && CLIFF_DURATION) {
@@ -11,10 +13,18 @@ async function main() {
       VESTED_ZERO_NFT_ADDRESS
     );
 
-    const tokenIds: number[] = [];
+    const lastTokenId = await vestContract.lastTokenId();
+
+    const tokenPromises = [];
+    for (let i = 0; i < lastTokenId; i++) {
+        tokenPromises.push(getAirdropTokenId(i, vestContract));
+    }
+
+    await Promise.all(tokenPromises);
+
     const linearDuration = tokenIds.map((item) => LINEAR_DURATION);
     const cliffDuration = tokenIds.map((item) => CLIFF_DURATION);
-
+    
     await vestContract.updateCliffDuration(
       tokenIds,
       linearDuration,
@@ -22,6 +32,15 @@ async function main() {
     );
   } else {
     throw new Error("Invalid arguments");
+  }
+}
+
+async function getAirdropTokenId(i: number, vestContract: VestedZeroNFT) {
+  const tokenToLockDetails = await vestContract.tokenIdToLockDetails(i);
+  if (tokenToLockDetails.category === 3n) {
+    if (tokenToLockDetails.cliffDuration > 86400n * 90n) {
+      tokenIds.push(i);
+    }
   }
 }
 
