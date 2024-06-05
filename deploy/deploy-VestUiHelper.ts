0e32af86b02434e2b1638b2d29fc63c2d73e6ff6
oxoxDev <nikhil@zerolend.xyz>
2024-06-06 03:14:49 +0530

    update deployment script

--- deploy/deploy-VestUiHelper.ts
+++ deploy/deploy-VestUiHelper.ts
@@ -4,16 +4,20 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import dotenv from "dotenv";
 dotenv.config();
 
-const VESTED_NFT_ADDRESS = "";
-const OMNICHAIN_STAKING_TOKEN = "";
-const OMNICHAIN_STAKING_LP = "";
+const VESTED_NFT_ADDRESS = "0x9FA72ea96591e486FF065E7C8A89282dEDfA6C12";
+const OMNICHAIN_STAKING_TOKEN = "0xf374229a18ff691406f99CCBD93e8a3f16B68888";
+const OMNICHAIN_STAKING_LP = "0xf36F8089D7dDc4522a1c10C8dA41555aCDcCCb4D";
 
 const deployAirdropRewarder = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  if (VESTED_NFT_ADDRESS.length && OMNICHAIN_STAKING_TOKEN.length && OMNICHAIN_STAKING_LP.length) {
+  if (
+    VESTED_NFT_ADDRESS.length &&
+    OMNICHAIN_STAKING_TOKEN.length &&
+    OMNICHAIN_STAKING_LP.length
+  ) {
     const vestUIDeployment = await deploy("VestedZeroUiHelper", {
       from: deployer,
       contract: "VestedZeroUiHelper",
@@ -22,20 +26,23 @@ const deployAirdropRewarder = async function (hre: HardhatRuntimeEnvironment) {
         proxyContract: "OpenZeppelinTransparentProxy",
         execute: {
           methodName: "initialize",
-          args: [VESTED_NFT_ADDRESS, OMNICHAIN_STAKING_TOKEN, OMNICHAIN_STAKING_LP],
+          args: [
+            VESTED_NFT_ADDRESS,
+            OMNICHAIN_STAKING_TOKEN,
+            OMNICHAIN_STAKING_LP,
+          ],
         },
       },
       autoMine: true,
       log: true,
     });
-    
+
     await hre.run("verify:verify", {
       address: vestUIDeployment.address,
     });
   } else {
     throw new Error("Invalid address for vest");
   }
-
 };
 
 deployAirdropRewarder.tags = ["VestUiHelper"];
